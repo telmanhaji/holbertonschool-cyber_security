b4c8d96ec541bdd5ee00079cb7b3a4ce9996f07e
telmanhaji <ibliandr@gmail.com>
2026-04-26 09:43:23 +0400

    commited 3-comprehensive_scan.sh to the project

--- network_security/0x07_nmap_post_port_scan_scripting/3-comprehensive_scan.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/3-comprehensive_scan.sh
@@ -1,2 +1,17 @@
 #!/bin/bash 
-nmap $1 --script http-vuln-cve2017-5638,ssl-enum-ciphers,ftp-anon -oN comprehensive_scan_results.txt
+if [ -z "$1" ]; then
+    echo "Usage: $0 <target>"
+    exit 1
+fi
+
+TARGET=$1
+OUTPUT="comprehensive_scan_results.txt"
+
+echo "Starting comprehensive security audit on $TARGET..."
+echo "This may take a while. Sit back, I'm analyzing..."
+
+sudo nmap -p 21,80,443,8080,8443 \
+    --script http-vuln-cve2017-5638,ssl-enum-ciphers,ftp-anon \
+    -oN "$OUTPUT" "$TARGET"
+
+echo "Audit finished. Findings saved to $OUTPUT"
