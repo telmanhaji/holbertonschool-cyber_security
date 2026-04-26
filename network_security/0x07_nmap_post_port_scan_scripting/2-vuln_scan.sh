26623628e09c64afb79bd15374acbe39305e5fac
telmanhaji <ibliandr@gmail.com>
2026-04-26 09:41:08 +0400

    commited 2-vuln_scan.sh to the project

--- network_security/0x07_nmap_post_port_scan_scripting/2-vuln_scan.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/2-vuln_scan.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-nmap $1 --script http-vuln-cve2017-5638 -oN vuln_scan_results.txt
+if [ -z "$1" ]; then
+    echo "Usage: $0 <target>"
+    exit 1
+fi
+
+TARGET=$1
+OUTPUT_FILE="vuln_scan_results_${TARGET}.txt"
+
+echo "Checking for CVE-2017-5638 on $TARGET..."
+echo "Results will be saved to $OUTPUT_FILE"
+
+sudo nmap -p 80,443,8080 --script http-vuln-cve2017-5638 "$TARGET" -oN "$OUTPUT_FILE"
+
+echo "Scan complete. Check $OUTPUT_FILE for details."
