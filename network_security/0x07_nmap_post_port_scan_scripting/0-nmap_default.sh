124fd38df01c4ab1202caf5b616a88126153a35c
telmanhaji <ibliandr@gmail.com>
2026-04-26 09:36:23 +0400

    commited 0-nmap_default.sh to the project

--- network_security/0x07_nmap_post_port_scan_scripting/0-nmap_default.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/0-nmap_default.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-nmap $1 --script default
+if [ -z "$1" ]; then
+    echo "Usage: $0 <target>"
+    exit 1
+fi
+
+if [ "$EUID" -ne 0 ]; then 
+  echo "Warning: Please run as root for full NSE functionality."
+fi
+
+echo "Starting default NSE scan for target: $1"
+nmap -sV --script default "$1"
